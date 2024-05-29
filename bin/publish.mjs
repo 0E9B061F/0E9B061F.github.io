3f495aff71eae3a2a0d4390f64ffc9b95fc5e18d
0E9B061F <0E9B061F@protonmail.com>
2024-05-29 04:25:11 -0400

    add publish script

---
+++ bin/publish.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { existsSync } from "node:fs"
+import { readFile } from "node:fs/promises"
+import { fileURLToPath } from "node:url"
+import { dirname, join } from "node:path"
+import { execSync } from "node:child_process"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const build = join(__dirname, "..", "docs")
+const rcfile = join(__dirname, "..", "remote.json")
+
+if (!existsSync(rcfile)) {
+  throw new Error("remote.json missing")
+} else if (!existsSync(build)) {
+  throw new Error("build is missing, nothing to publish")
+} else {
+  const conf = JSON.parse(await readFile(rcfile))
+  const res = execSync(`rsync -av --delete -e "ssh -p ${conf.port}" docs/* "${conf.dest}"`, {encoding: "utf-8"})
+  console.log(res)
+}
